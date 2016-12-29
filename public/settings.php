41a304185dd90078fb34c0c95f13013969df7021
Lucas Guima <lucasguima.felipe@gmail.com>
2016-12-29 18:51:21 -0200

    Ajustada a nomenclatura das credenciais do Google

--- public/settings.php
+++ public/settings.php
@@ -20,7 +20,7 @@ $drive_enabled = strpos($_SESSION['access_token']['scope'], Google_Service_Drive
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="google-signin-client_id" content="200909192167-qck6j2hh0kma3cg1h95p6n2k1e1o3jqd.apps.googleusercontent.com">
+    <meta name="google-signin-client_id" content="<?php echo GOOGLE_CLIENT_ID; ?>">
     <title>Configurações | Carteiro</title>
 
     <!-- Bootstrap -->
@@ -121,7 +121,7 @@ $drive_enabled = strpos($_SESSION['access_token']['scope'], Google_Service_Drive
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
     <script type="text/javascript">
-        oauth_client_id = '<?php echo OAUTH_CLIENT_ID; ?>';
+        google_client_id = '<?php echo GOOGLE_CLIENT_ID; ?>';
     </script>
     <script src="https://apis.google.com/js/platform.js?onload=init" async defer></script>
 </body>
